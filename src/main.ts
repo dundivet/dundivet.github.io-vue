09225c0ba18d1fe2e57dcd37e084d0f81455856f
Carlos J <dundivet@gmail.com>
2019-12-09 13:51:29 -0500

    Added bootstrap SCSS.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
+import './assets/scss/main.scss'
+
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
