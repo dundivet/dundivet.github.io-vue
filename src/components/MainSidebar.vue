90e973b3a3187957df26c43c2543f5341c796579
Carlos J <dundivet@gmail.com>
2019-12-10 09:58:24 -0500

    Hotfix collapsible menu on movile view

--- src/components/MainSidebar.vue
+++ src/components/MainSidebar.vue
@@ -1,24 +1,24 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+    <a class="navbar-brand js-scroll-trigger" href="/">
       <span class="d-block d-lg-none">Carlos J. Infante Ramos</span>
       <span class="d-none d-lg-block">
         <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/78827370_3125278544155291_5419594784421445632_o.jpg" alt="profile-pic">
       </span>
     </a>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    <b-button class="navbar-toggler" v-b-toggle="'navbarSupportedContent'"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-    </button>
+    </b-button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <b-collapse class="navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav">
         <li class="nav-item" v-for="link in links" :key="link">
           <router-link class="nav-link" :to="link.to" active-class="active">{{ link.label }}</router-link>
         </li>
       </ul>
-    </div>
+    </b-collapse>
   </nav>
 </template>
 
